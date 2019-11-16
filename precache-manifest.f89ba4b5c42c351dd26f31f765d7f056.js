f63176476667ae48b1703551e5162a61f07e9604
voss-e <jovicevicdev@gmail.com>
2019-11-16 16:07:14 +0100

    Updates

---
+++ precache-manifest.f89ba4b5c42c351dd26f31f765d7f056.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa882d101086eb69a700bd2d119b1ab",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "da3d64bae6f8f318e572",
+    "url": "/todo/static/css/main.0a38425e.chunk.css"
+  },
+  {
+    "revision": "1c929aaa6d73cacbf7e5",
+    "url": "/todo/static/js/2.df1a57af.chunk.js"
+  },
+  {
+    "revision": "da3d64bae6f8f318e572",
+    "url": "/todo/static/js/main.505810e8.chunk.js"
+  },
+  {
+    "revision": "fa403113ead66244d282",
+    "url": "/todo/static/js/runtime-main.0fecbaf7.js"
+  }
+]);
